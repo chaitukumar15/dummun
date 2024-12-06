9133821c34391feedfcb4b94229656b564912286
chaitukumar15 <108114098+chaitukumar15@users.noreply.github.com>
2024-12-06 13:13:40 +0530

    chfdh

--- index.js
+++ index.js
@@ -26,8 +26,8 @@ var uplaod=multer({storage:store})
 
 app.get("/product",uplaod.single("file"),(req,res)=>{
 
-    console.log(req.file);
-    var h=req.file.size*0.001;
+    // console.log(req.file);
+    // var h=req.file.size*0.001;
     res.send("hello")
 // res.send({
 //     n:h,
